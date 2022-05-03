c7963c8889d1325027af6311d4b1958207615ab3
marq4 <elapamarquina@hotmail.com>
2022-05-03 10:22:15 -0500

    Refactor code. Automatic linting.

--- random_clip_generator.py
+++ random_clip_generator.py
@@ -1,85 +1,127 @@
-# Random Video Clips Player for Win10. 
-# By marq_. 
-# Based on code by @author niknow. 
+"""
+Random video clips player for Win10.
+"""
 
-import subprocess
-from subprocess import Popen 
-import xml.etree.cElementTree as ET
 import os
 import random
+import subprocess
+import xml.etree.ElementTree as ET
+from subprocess import Popen
+
+XML_PLAYLIST_FILE = 'clips.xspf'
+SUBFOLDER = 'videos'
+NUMBER_OF_CLIPS = 4
+INTERVAL_MIN = 2
+INTERVAL_MAX = 2
+CURRENT_DIRECTORY = os.path.dirname( os.path.abspath(__file__) )
 
 
-xmlplaylistfile = "clips.xspf"
-subfolder = "videos"
-numclips = 10
-intervalmin = 4
-intervalmax = 8
+def prepend_line(filename: str, line: str) -> None:
+    """ Append line to beginning of file. """
+    with open(filename, 'r+', encoding='utf-8') as file:
+        content = file.read()
+        file.seek(0,0)
+        file.write( line.rstrip("\r\n") + "\n" + content )
 
 
-"""
-For all files under folder "videos",
-Do many times:
-    Select a video at random.
-    Select a starting point for it at random.
-    Select a duration between (min,max) sec at random.
-    Add this info to playlist file.
-Execute VLC once and pass it the xml as a playlist.
-To play prevoiusly generated playlists add them to batch file execvlc.bat.
-"""
+def list_files_subfolder() -> list:
+    """ Create a list of all files in SUBFOLDER (videos). """
+    return os.listdir(SUBFOLDER)
 
-# By @author eyquem
-def line_prepender(filename,line):
-    with open(filename,'r+') as f:
-        content=f.read()
-        f.seek(0,0)
-        f.write(line.rstrip('\r\n')+'\n'+content)
-#
 
-ffprobestr = ("ffprobe -v error -select_streams v:0 -show_entries stream=duration " +
-              "-of default=noprint_wrappers=1:nokey=1 ")
+def select_video_at_random(list_of_files: list) -> str:
+    """ Choose a video. :return: Video filename with Win full path. """
+    subpath = os.path.join(CURRENT_DIRECTORY, SUBFOLDER)
+    selected = random.randint(0, len(list_of_files) - 1)
+    return ( os.path.join(subpath, list_of_files[selected]) ).replace("\\", "/")
 
-# Create a list of all files in folder 'videos':
-Videos = os.listdir(subfolder)
 
-playlist = ET.Element("playlist", version="1", xmlns="http://xspf.org/ns/0/")
-trackList = ET.SubElement(playlist, "trackList")
+def get_video_duration(num_to_log: int, video: str) -> int:
+    """ Extract video duration with ffprobe and subprocess.Popen.
+        :return: Video duration in seconds. """
+    ffprobe_str = "ffprobe -v error -select_streams v:0 \
+        -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1"
+    command = f"{ffprobe_str} {video} "
+    with Popen(command, stdout=subprocess.PIPE, shell=True) as process:
+        (out, err) = process.communicate()
+    if err:
+        print(f"Process error: [{str(err)}]. Iteration: {num_to_log}. ")
+    result = ( ( ( ( str(out) ).split('.', maxsplit=1) )[0] ).split("b'") )[1]
+    return int(result)
 
-for q in range(numclips):
-    # Choose a random file number:
-    selected = random.randint(0, len(Videos) - 1)
 
-    # Create path to current video file:
-    THIS_FOLDER = os.path.dirname(os.path.abspath(__file__)) 
-    subpath = os.path.join(THIS_FOLDER, subfolder) 
-    videofile = os.path.join(subpath, Videos[selected]).replace('\\', '/') 
+def choose_starting_point(video_length: int) -> int:
+    """ Choose beginning of clip.
+    :return: Starting point from beginning of video to end of video - max. """
+    return random.randint(0, int(video_length) - INTERVAL_MAX)
 
-    # Get the duration of that video:
-    getdurationcmd = ffprobestr + videofile
-    proc = subprocess.Popen(getdurationcmd, stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    duration = str(out).split(".")[0].split("b'")[1]
 
-    # Select a starting point from begining of video to end of video - max:
-    beginat = random.randint(0, int(duration) - intervalmax)
+def add_clip_to_tracklist(track_list: ET.Element, \
+    video: str, start: int, end: int) -> None:
+    """ Add clip (track) to playlist.trackList sub element tree and mute.
+        :param: track_list: Contains the clips.
+        :param: video: The name of the video file to be cut.
+        :param: start: Begin clip from.
+        :param: end: Stop clip at.
+    """
+    track = ET.SubElement(track_list, 'track')
+    ET.SubElement(track, 'location').text = f"file:///{video}"
+    extension = ET.SubElement(track, 'extension', \
+        application='http://www.videolan.org/vlc/playlist/0')
+    ET.SubElement(extension, 'vlc:option').text = f"start-time={str(start)}"
+    ET.SubElement(extension, 'vlc:option').text = f"stop-time={str(end)}"
+    ET.SubElement(extension, 'vlc:option').text = 'no-audio'
 
-    # Select a clip length:
-    cliplength = random.randint(intervalmin, intervalmax)
-    playto = beginat + cliplength
 
-    # Add the clip to the playlist:
-    track = ET.SubElement(trackList, "track")
-    ET.SubElement(track, "location").text = "file:///" + videofile
-    extension = ET.SubElement(track, "extension", application="http://www.videolan.org/vlc/playlist/0")
-    ET.SubElement(extension, "vlc:option").text = "start-time=" + str(beginat)
-    ET.SubElement(extension, "vlc:option").text = "stop-time=" + str(playto)
-    ET.SubElement(extension, "vlc:option").text = "no-audio"
-#
+def create_xml_file(playlist_et: ET.Element) -> None:
+    """ Finally write the playlist tree element as an xspf file to disk. """
+    ET.ElementTree(playlist_et).write(XML_PLAYLIST_FILE)
+    prepend_line(XML_PLAYLIST_FILE, '<?xml version="1.0" encoding="UTF-8"?>')
 
-# Create xml file:
-ET.ElementTree(playlist).write(xmlplaylistfile)
-line_prepender(xmlplaylistfile, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
 
-# Open VLC with playlist:
-executable = os.path.join(THIS_FOLDER, 'exevlc.bat') 
-p = subprocess.Popen([executable, THIS_FOLDER + '/' + xmlplaylistfile])
+def generate_random_video_clips_playlist(video_list: list) -> ET.Element:
+    """
+    * Create playlist as an xml element tree.
+    * Create tracklist as subelement of playlist. This contains the clips.
+    * For each clip to be generated:
+        + Select a video at random.
+        + Choose beginning and end of clip from selected video.
+        + Add clip to playlist.
+    """
 
+    playlist = ET.Element("playlist", version="1", xmlns="http://xspf.org/ns/0/")
+    tracks = ET.SubElement(playlist, "trackList")
+
+    for iteration in range(NUMBER_OF_CLIPS):
+        video_file = select_video_at_random(video_list)
+        duration = get_video_duration(iteration, video_file)
+
+        begin_at = choose_starting_point(duration)
+        clip_length = random.randint(INTERVAL_MIN, INTERVAL_MAX)
+        play_to = begin_at + clip_length
+
+        add_clip_to_tracklist(tracks, video_file, begin_at, play_to)
+
+    return playlist
+
+
+def execute_vlc() -> None:
+    """ Call VLC only once and pass it the xspf playlist. """
+    executable = os.path.join(CURRENT_DIRECTORY, 'exevlc.bat')
+    with Popen([executable, f"{CURRENT_DIRECTORY}/{XML_PLAYLIST_FILE}"]):
+        pass
+
+
+def main():
+    """
+    * Get list of videos.
+    * Generate an xml playlist with random clips from those videos.
+    * Run VLC with that playlist.
+    """
+    files = list_files_subfolder()
+    top_element = generate_random_video_clips_playlist(files)
+    create_xml_file(top_element)
+    execute_vlc()
+
+if __name__ == "__main__":
+    main()
--- sanitize_video_titles.py
+++ sanitize_video_titles.py
@@ -1,16 +1,15 @@
-# Remove spaces and special characters from file names. 
+""" Remove spaces and special characters from file names. """
 
-import os 
-import re 
+import os
+import re
 
-for original in os.listdir("."): 
+# ignore = [os.path.basename(__file__), 'random_clip_generator.py', 'exevlc.bat']
+
+for original in os.listdir("."):
     if original == os.path.basename(__file__) \
-            or original == 'random_clip_generator.py' \
-            or original == 'exevlc.bat': 
-        continue 
-    sanitized = re.sub('[^a-zA-Z0-9\.]', '', original)
-    if sanitized != original: 
+        or original == 'random_clip_generator.py' \
+        or original == 'exevlc.bat':
+        continue
+    sanitized = re.sub(r"[^a-zA-Z0-9\.]", '', original)
+    if sanitized != original:
         os.rename(original, sanitized)
-#
-
-
